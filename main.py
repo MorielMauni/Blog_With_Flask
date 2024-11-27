9cc1f5e7bfff4a239b7adf4af057143228099dae
Moriel Mauni <moriel.ma0@gmail.com>
2024-11-27 19:10:32 +0200

    updates to the blog

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 # ------------------- API For Blog ---------------------- #
 url = "https://api.npoint.io/c3ddac6b9d573683fadd"
 posts = requests.get(url=url).json()
 
+# ------------------- Mail ---------------------- #
+mail = "<your@mail.com>"
+passwd = "<password>"  # It's the 'app password' from the password given by Gmail
 
+# ------------------- Flask ---------------------- #
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,9 +21,27 @@ def home_page():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()  # Encrypt on the fly
+            connection.login(user=mail, password=passwd)  # Log into the mail
+            # Prepare the message content, encoding in utf-8
+            message = f"Subject: New Message from website\n\nContact request was send on the website by {name}:\n{message}\ncontect with Email {email} / Phone number {phone}"
+            connection.sendmail(from_addr=mail,
+                                to_addrs="<send_to@mail.com>",
+                                msg=message.encode('utf-8'))  # Explicitly encode the message as UTF-8
+
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
